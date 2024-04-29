18800479d9b0d378c1dcd9a9e2828a433d1b8aaa
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-04-28 22:43:15 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -2,17 +2,24 @@
 .globl init_student
 
 init_student:
+    # Ensure address in $a0 is word-aligned
+    andi $t2, $a0, 3
+    bnez $t2, handle_misalignment
+
     li $t0, 0                 
     sll $t1, $a1, 10          
     or $t0, $t0, $t1          
     or $t0, $t0, $a2          
 
     sw $t0, 0($a0)            
-
     sw $a3, 4($a0)            
 
     jr $ra                    
 
+handle_misalignment:
+    addi $a0, $a0, 4
+    andi $a0, $a0, -4
+    j init_student                            
 	
 print_student:
 	jr $ra
