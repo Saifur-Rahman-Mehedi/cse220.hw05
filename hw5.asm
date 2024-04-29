84bf6aa42e1c23746e0be0ae7c014b5512cb1689
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-04-29 00:37:32 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -1,23 +1,39 @@
+.data
+.align 2 
+_record: .space 8  
+
 .text
+.globl main
 .globl init_student
+
+main:
+    la $a0, _record  
+    li $a1, 3126375  
+    li $a2, 314      
+    la $a3, _name    
+
+    jal init_student  
+
+    li $v0, 10
+    syscall  # Exit the program.
 
 init_student:
     andi $t0, $a0, 3
-    bnez $t0, align_address
+    bnez $t0, align_address  
 
 continue:
-    sll $t1, $a1, 10
-    or $t0, $t1, $a2
+    sll $t1, $a1, 10  
+    or $t0, $t1, $a2  
 
-    sw $t0, 0($a0)
-    sw $a3, 4($a0)
+    sw $t0, 0($a0)  
+    sw $a3, 4($a0)  
 
-    jr $ra
+    jr $ra 
 
 align_address:
-    addi $a0, $a0, 4
-    andi $a0, $a0, 0xFFFC
-    j continue
+    addi $a0, $a0, 4  
+    andi $a0, $a0, 0xFFFC  
+    j continue  
 
                           
 	
