0e7e04dec1c9527eb594147b39a44e364d460577
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-04-29 21:23:17 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -19,37 +19,9 @@ init_student:
     sw $a2, 4($a3)             # Store the address of the name right after the ID and credits
     jr $ra                     # Return to the caller
 
-.globl print_student
-# Argument:
-# $a0 - Address of Record (struct student *)
-
 print_student:
-    lw $t0, 0($a0)             # Load the combined ID and credits
-    li $t1, 0x3FFFFF           # Mask for ID
-    and $t2, $t0, $t1          # Extract ID
-    srl $t3, $t0, 22           # Shift right to get credits
 
-    li $v0, 1                  # Prepare to print ID
-    move $a0, $t2
-    syscall
-
-    li $v0, 11                 # Print space
-    li $a0, 32
-    syscall
-
-    li $v0, 1                  # Prepare to print Credits
-    move $a0, $t3
-    syscall
-
-    li $v0, 11                 # Print space
-    li $a0, 32
-    syscall
-
-    lw $a0, 4($a0)             # Load the address of the name
-    li $v0, 4                  # Prepare to print name
-    syscall
-
-    jr $ra                     # Return from the function
+    jr $ra                  
 
 
 	
