7fbbb345359c43fcb89c95d7d22653b1800bc4a5
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-04-29 20:48:59 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -1,17 +1,28 @@
-.data
-record: .space 8
-
 .text
-.globl init_student
 init_student:
-    sll $t0, $a1, 22
-    or $t0, $t0, $a0
-    sw $t0, 0($a3)
-    sw $a2, 4($a3)
-    jr $ra             
+    sll $t0, $a1, 22            # Shift credits left to make space for ID
+    or $t0, $t0, $a0            # Combine ID and credits
+    sw $t0, 0($a3)              # Store ID and credits at the start of the record
+    sw $a2, 4($a3)              # Store the address of the name
+    jr $ra                      # Return from the function      
                                          
+.text
 print_student:
-	jr $ra
+    lw $t0, 0($a0)              # Load the ID and credits
+    andi $t1, $t0, 0x3FFFFF     # Mask to get only ID
+    srl $t2, $t0, 22            # Shift right to get credits
+    li $v0, 1                   # Prepare to print ID
+    move $a0, $t1
+    syscall
+    li $v0, 1                   # Prepare to print credits
+    move $a0, $t2
+    syscall
+    lw $t3, 4($a0)              # Load the address of the name
+    li $v0, 4                   # Prepare to print name
+    move $a0, $t3
+    syscall
+    jr $ra                      # Return from the function
+
 	
 init_student_array:
 	jr $ra
