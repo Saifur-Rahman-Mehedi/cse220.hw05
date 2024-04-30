92b8aa8e539378f2ae3bd77d9e83c2d4033ce13a
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-04-29 20:44:54 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -1,26 +1,14 @@
 .data
-student_record:
-    .word 0    # ID
-    .word 0    # Credits
-    .space 128 # Name
+record: .space 8
 
 .text
-
+.globl init_student
 init_student:
-    sw $a0, student_record
-    sw $a1, student_record + 4
-    
-    la $t0, student_record + 8  
-    la $t1, 0($a2)              
-    name_copy:
-        lb $t2, 0($t1)         
-        sb $t2, 0($t0)         
-        beqz $t2, end_copy     
-        addiu $t0, $t0, 1      
-        addiu $t1, $t1, 1      
-        j name_copy             
-    end_copy:
-    jr $ra                     
+    sll $t0, $a1, 22
+    or $t0, $t0, $a0
+    sw $t0, 0($a3)
+    sw $a2, 4($a3)
+    jr $ra             
                                          
 print_student:
 	jr $ra
