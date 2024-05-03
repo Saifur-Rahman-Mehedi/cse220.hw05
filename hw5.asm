30bfbff04852feb6f6923aef5a6672baa519efe3
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-05-03 17:43:56 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -1,25 +1,23 @@
 .data
 .align 2
-_record: .space 12  # Ensure enough space for ID (4 bytes), Credits (4 bytes), and pointer (4 bytes)
+unique_record: .space 12  
 
 .text
 .globl init_student
 init_student:
-    # Store the ID, credits, and the address of the name string into the record
-    sw $a0, 0($a3)      # Store the ID at the beginning of the record
-    sw $a1, 4($a3)      # Store the credits next to the ID
-    sw $a2, 8($a3)      # Store the pointer to the name string
-    jr $ra              # Return to the caller
-
+    sw $a0, 0($a3)      # Store the ID at the beginning of the record.
+    sw $a1, 4($a3)      # Store the credits next to the ID.
+    sw $a2, 8($a3)      # Store the address of the name string next.
+    jr $ra              # Return to the caller.
+                    
 .text
 .globl print_student
 print_student:
-    # Load student record fields from memory
     lw $t0, 0($a0)      # Load student ID
     lw $t1, 4($a0)      # Load credits
     lw $t2, 8($a0)      # Load address of the name string
 
-    # Print the ID
+    # Print ID
     move $a0, $t0
     li $v0, 1           # syscall for printing integer
     syscall
@@ -29,7 +27,7 @@ print_student:
     li $v0, 11          # syscall for printing character
     syscall
 
-    # Print the credits
+    # Print credits
     move $a0, $t1
     li $v0, 1           # syscall for printing integer
     syscall
@@ -39,14 +37,13 @@ print_student:
     li $v0, 11          # syscall for printing character
     syscall
 
-    # Print the name
+    # Print name
     move $a0, $t2
     li $v0, 4           # syscall for printing string
     syscall
 
-    jr $ra              # Return to the caller
-  
-	
+    jr $ra              # Return to the caller.
+
 init_student_array:
 	jr $ra
 	
