74bfc8751c31677820cd86a37a036b7cdd649d49
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-05-03 20:17:45 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -43,34 +43,34 @@ print_student:
 .globl init_student_array
 init_student_array:
     lw $s0, 0($sp)
-    move $t0, $a1
-    move $t1, $a2
-    move $t2, $a3
+    move $t0, $a1  # Pointer to ID list
+    move $t1, $a2  # Pointer to credits list
+    move $t2, $a3  # Pointer to names array
 
     li $t3, 0
-    blez $a0, init_exit
+    blez $a0, init_exit  # If number of students is zero, exit
 
 init_loop:
-    beq $t3, $a0, init_exit
+    beq $t3, $a0, init_exit  # Exit loop if all students are processed
 
-    lw $a0, 0($t0)
-    lw $a1, 0($t1)
-    la $a2, ($t2)
+    lw $a0, 0($t0)  # Load student ID
+    lw $a1, 0($t1)  # Load credits
+    move $a2, $t2   # Address of the current name
 
-    la $a3, ($s0)
-    jal init_student
+    add $a3, $s0, $zero  # Address to store the record
+    jal init_student      # Initialize student record
 
-    addiu $t0, $t0, 4
-    addiu $t1, $t1, 4
-    addiu $s0, $s0, 12
+    addiu $t0, $t0, 4     # Move to the next ID
+    addiu $t1, $t1, 4     # Move to the next credits
+    addiu $s0, $s0, 12    # Move to the next record space
 
-    find_next_name:
+    find_next_name:       # Parse through the name string
         lbu $t4, 0($t2)
-        beqz $t4, update_name_pointer
+        beqz $t4, update_name_pointer  # If char is zero, move to next name
         addiu $t2, $t2, 1
         j find_next_name
     update_name_pointer:
-        addiu $t2, $t2, 1
+        addiu $t2, $t2, 1  # Move past the null terminator
 
     addiu $t3, $t3, 1
     j init_loop
