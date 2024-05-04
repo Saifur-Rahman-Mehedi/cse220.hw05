0df588b5c7b4e55b845b9ef19bb3ecbbd08a5e98
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-05-03 20:02:02 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -39,9 +39,43 @@ print_student:
 
     jr $ra
 
-
+.text
+.globl init_student_array
 init_student_array:
- 
+    lw $s0, 0($sp)
+    move $t0, $a1
+    move $t1, $a2
+    move $t2, $a3
+
+    li $t3, 0
+    blez $a0, init_exit
+
+init_loop:
+    beq $t3, $a0, init_exit
+
+    lw $a0, 0($t0)
+    lw $a1, 0($t1)
+    la $a2, 0($t2)
+
+    la $a3, 0($s0)
+    jal init_student
+
+    addiu $t0, $t0, 4
+    addiu $t1, $t1, 4
+    addiu $s0, $s0, 12
+
+    find_next_name:
+        lbu $t4, 0($t2)
+        beqz $t4, update_name_pointer
+        addiu $t2, $t2, 1
+        j find_next_name
+    update_name_pointer:
+        addiu $t2, $t2, 1
+
+    addiu $t3, $t3, 1
+    j init_loop
+
+init_exit:
     jr $ra
 
 insert:
