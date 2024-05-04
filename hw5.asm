de9b2bf519c8a81e31f6c55c759ea3c051801fd0
Saifur-Rahman-Mehedi <157724313+Saifur-Rahman-Mehedi@users.noreply.github.com>
2024-05-03 20:48:58 -0400

    Update hw5.asm

--- hw5.asm
+++ hw5.asm
@@ -5,9 +5,9 @@ unique_record: .space 12
 .text
 .globl init_student
 init_student:
-    sw $a0, 0($a3)
-    sw $a1, 4($a3)
-    sw $a2, 8($a3)
+    sw $a0, 0($a3)  # Store ID
+    sw $a1, 4($a3)  # Store credits
+    sw $a2, 8($a3)  # Store address of name
     jr $ra
 
 .text
@@ -17,6 +17,7 @@ print_student:
     lw $t1, 4($a0)
     lw $t2, 8($a0)
 
+    # Print ID
     move $a0, $t0
     li $v0, 1
     syscall
@@ -25,6 +26,7 @@ print_student:
     li $v0, 11
     syscall
 
+    # Print credits
     move $a0, $t1
     li $v0, 1
     syscall
@@ -33,8 +35,14 @@ print_student:
     li $v0, 11
     syscall
 
-    move $a0, $t2
+    # Print name
+    la $a0, 0($t2)
     li $v0, 4
+    syscall
+
+    # New line after each record
+    li $a0, '\n'
+    li $v0, 11
     syscall
 
     jr $ra
@@ -42,47 +50,39 @@ print_student:
 .text
 .globl init_student_array
 init_student_array:
-    lw $s0, 0($sp)
-    move $t0, $a1  
-    move $t1, $a2  
-    move $t2, $a3  
+    lw $s0, 0($sp)  # Address of record space
+    move $t0, $a1   # ID list
+    move $t1, $a2   # Credits list
+    move $t2, $a3   # Names array
 
-    li $t3, 0
-    blez $a0, init_exit  
+    li $t3, 0       # Counter for number of students
+    blez $a0, init_exit  # Exit if no students
 
 init_loop:
-    beq $t3, $a0, init_exit  
+    beq $t3, $a0, init_exit  # All students processed
 
-    lw $a0, 0($t0)  
-    lw $a1, 0($t1)  
-    move $a2, $t2   
+    lw $a0, 0($t0)  # Load ID
+    lw $a1, 0($t1)  # Load credits
+    la $a2, 0($t2)  # Start of name string
 
-    add $a3, $s0, $zero  
-    jal init_student      
+    la $a3, 0($s0)  # Target record
+    jal init_student  # Initialize student
 
-    addiu $t0, $t0, 4     
-    addiu $t1, $t1, 4     
-    addiu $s0, $s0, 12    
+    addiu $t0, $t0, 4  # Next ID
+    addiu $t1, $t1, 4  # Next credits
+    addiu $s0, $s0, 12 # Next record address
 
-    find_next_name:       
+    # Find next name string
+    find_next_name:
         lbu $t4, 0($t2)
-        beqz $t4, update_name_pointer  
+        beqz $t4, update_name_pointer
         addiu $t2, $t2, 1
         j find_next_name
     update_name_pointer:
-        addiu $t2, $t2, 1  
+        addiu $t2, $t2, 1  # Skip null terminator
 
     addiu $t3, $t3, 1
     j init_loop
 
 init_exit:
-    jr $ra
-
-insert:
-    jr $ra
-    
-search:
-    jr $ra
-
-delete:
     jr $ra
